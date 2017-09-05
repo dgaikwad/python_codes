2f2b67d26a03c7ec1069ca842c2bc1561a85b576
Devidas Gaikwad <devidasgkwd@gmail.com>
2017-09-05 13:12:18 +0530

    fixed the code

--- sum_of_list_ele.py
+++ sum_of_list_ele.py
@@ -2,7 +2,7 @@
 def sum_list(list1):
     sum=0
     count=len(list1)
-    for i in range(1,count+1):
+    for i in list1:
         print(i)
         sum=sum+i
     print("Sum of all list elememts={0}".format(sum))
