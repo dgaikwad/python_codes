e9e1fc51ed58dc717ffbed0ae6a658424dffd9cf
Devidas Gaikwad <devidasgkwd@gmail.com>
2017-09-12 23:53:26 +0530

    binary search program

--- binary_search.py
+++ binary_search.py
@@ -18,8 +18,8 @@ def binary_search(list1, data):
             else:
                 last=mid - 1
     return found    
-    
-testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]    
-print(binary_search(testlist, 3))
-print(binary_search(testlist, 32))
+if __name__ == "__main__"    :
+    testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]    
+    print(binary_search(testlist, 3))
+    print(binary_search(testlist, 32))
 
